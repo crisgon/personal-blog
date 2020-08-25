e120d8a439ab4a76ea537b76bdddfc1c424b1719
Cristiano <cristiano.goncalves@cubos.io>
2020-08-24 21:57:51 -0300

    feature: make post list have 100vh.

--- src/components/PostList/styled.js
+++ src/components/PostList/styled.js
@@ -3,6 +3,7 @@ import media from "styled-media-query";
 
 export const ListWrapper = styled.section`
   padding: 0.5rem 1rem;
+  min-height: calc(100vh - 50px);
 
   ${media.greaterThan("medium")`
     body#grid & {
